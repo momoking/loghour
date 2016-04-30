a608492733a6a66ef82cde1779ae9e89eb3cefc2
momoking <1.haiyuezhang@gmail.com>
2016-04-30 18:43:04 -0400

    add color

--- index.js
+++ index.js
@@ -3,29 +3,31 @@
 var conf = require('./package');
 var moment = require('moment');
 var fs = require('fs');
+var colors = require('colors/safe');
 
 fs.readFile(conf.logfile, 'utf-8', function(err, data) {
 	var timeNow = moment();
 	var timeNowFormatted = timeNow.format(conf.timeFormat);
-	var feedback;
+	var duration;
 
 	if (!err) {
 		var lines = data.trim().split('\n');
-		var lastLine = lines.splice(-1)[0];		
-		var lastEntryTime = moment(lastLine, conf.timeFormat);
-		if (lastEntryTime && lastEntryTime.isValid()) {
-			feedback = moment.duration(lastEntryTime.diff(timeNow)).humanize(true) + ' since last entry';	
-		}		
+		var lastLine = lines.splice(-1)[0];
+		var previousEntryTime = moment(lastLine, conf.timeFormat);
+		if (previousEntryTime && previousEntryTime.isValid()) {
+			duration = moment.duration(previousEntryTime.diff(timeNow)).humanize(true);
+		}
 	}
 
 	fs.appendFile(conf.logfile, timeNowFormatted + '\n', function(err) {
 		if (err) {
 			return console.log('error writting:', err);
 		}
-		console.log(timeNowFormatted, '-> saved');
-		if (feedback) {
-			console.log(feedback);
+		console.log(colors.green('SAVED') + '\t' + timeNowFormatted);
+		if (duration) {			
+			console.log(colors.inverse('PREV\t' + previousEntryTime.format(conf.timeFormat) + ' (' + duration + ')'));
 		}
+		
 	});
 
 });
