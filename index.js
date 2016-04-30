4b3bd21682f8b2c75f30e3836eb667c3fb3fce95
momoking <1.haiyuezhang@gmail.com>
2016-04-30 16:12:23 -0400

    core

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+var conf = require('./package');
+var moment = require('moment');
+var fs = require('fs');
+
+fs.readFile(conf.logfile, 'utf-8', function(err, data) {
+	var timeNow = moment();
+	var timeNowFormatted = timeNow.format(conf.timeFormat);
+	var feedback;
+	if (!err) {
+		var lines = data.trim().split('\n');
+		var lastLine = lines.splice(-1)[0];
+		var lastEntryTime = moment(lastLine, conf.timeFormat);
+		feedback = moment.duration(lastEntryTime.diff(timeNow)).humanize(true) + ' since last entry';
+	}
+
+	fs.appendFile(conf.logfile, timeNowFormatted + '\n', function(err) {
+		if (err) {
+			return console.log(err);
+		}
+		console.log(timeNowFormatted, '-> saved');
+		if (feedback) {
+			console.log(feedback);
+		}
+	});
+
+});
